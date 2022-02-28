1f6047cee83f26e11e06b3e6afd4ee0d471ef32d
vuvanminh267nt <vuvanminh267nt@gmail.com>
2022-02-28 09:27:47 +0700

    init

---
+++ routers/posts.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
+
+const router = express.Router()
+
+router.get('/', getPosts)
+
+router.post('/', createPost)
+
+router.put('/', updatePost)
+
+router.delete('/', deletePost)
+
+export default router
